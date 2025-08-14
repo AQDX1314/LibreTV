045d67deccdbbbda4f8f9677e9570e39839f4147
AQDX1314 <zhouhao643@gmail.com>
2025-08-14 22:17:46 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -110,6 +110,10 @@ const API_SITES = {
         api: 'https://ikunzyapi.com/api.php/provide/vod',
         name: 'iKun资源'
     },
+        "maotaizy": {
+      "api": "https://caiji.maotaizy.cc/api.php/provide/vod",
+      "name": "茅台资源"
+    },
     lzi: {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
